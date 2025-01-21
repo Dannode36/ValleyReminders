d387880d68373d5f3b8a7dbe16eed2bfb2220011
Dannode36 <crafty_dan@outlook.com>
2025-01-21 11:31:49 +1100

    check

--- src/ReminderMenu.cs
+++ src/ReminderMenu.cs
@@ -6,6 +6,9 @@ using System.Text.RegularExpressions;
 using SMUI.Elements;
 using SMUI.Elements.Pickers;
 using ParameterData = System.Collections.Generic.KeyValuePair<string, ValleyReminders.ParameterType>;
+using SMUI;
+using Microsoft.Xna.Framework.Input;
+using SMUI.SDV;
 
 namespace ValleyReminders
 {
@@ -16,13 +19,12 @@ namespace ValleyReminders
         CREATE
     }
 
-    class ReminderMenu : IClickableMenu
+    class ReminderMenu : SmuiMenu
     {
         public const int Width = 832;
         public const int Height = 616;
         public static Vector2 CenterOffset => new((Game1.uiViewport.Width - Width) / 2, (Game1.uiViewport.Height - Height) / 2);
 
-        private RootElement rootElement = new();
         private ReminderMenuState state = ReminderMenuState.LIST;
 
         //LIST
@@ -31,21 +33,48 @@ namespace ValleyReminders
         bool reminderListDirty = false;
 
         //EDIT
-        private StaticContainer reminderEditPage = new();
+        private Panel reminderEditPage = new();
         private Reminder? selectedReminder = null;
 
         //CREATE
-        private StaticContainer reminderCreatePage = new();
+        private Panel reminderCreatePage = new();
         private Reminder? newReminder = null;
 
         public void CreateInterface(List<Reminder> reminders)
         {
             this.reminders = reminders;
+            Root = new();
 
             initialize((int)CenterOffset.X, (int)CenterOffset.Y, Width, Height, true);
             upperRightCloseButton.setPosition(new(CenterOffset.X + Width + 20, CenterOffset.Y - 40));
-            CreateStaticInterface();
-            UpdateListPage();
+
+            reminderListPage = new()
+            {
+                RowHeight = 100,
+                Size = new(Width, Height),
+                Position = CenterOffset,
+                RowPadding = 0
+            };
+
+            reminderEditPage = new()
+            {
+                Size = new(Width, Height),
+                Position = CenterOffset,
+            };
+
+            reminderCreatePage = new()
+            {
+                Size = new(Width, Height),
+                Position = CenterOffset,
+                OutlineColor = Color.White,
+            };
+
+            AddChild(reminderListPage);
+            AddChild(reminderEditPage);
+            AddChild(reminderCreatePage);
+
+            /*UpdateListPage();
+            UpdateCreatePage();*/
         }
 
         public void OnOpen()
@@ -54,42 +83,23 @@ namespace ValleyReminders
             state = ReminderMenuState.LIST;
             selectedReminder = null;
             newReminder = null;
-        }
-
-        private void CreateStaticInterface()
-        {
-            rootElement = new();
-            rootElement.AddChild(reminderListPage);
-            rootElement.AddChild(reminderEditPage);
-            rootElement.AddChild(reminderCreatePage);
+            CreateInterface(reminders);
         }
 
         public void UpdateListPage()
         {
-            rootElement.RemoveChild(reminderListPage);
-
-            reminderListPage = new()
-            {
-                RowHeight = 100,
-                Size = new(Width, Height),
-                LocalPosition = CenterOffset,
-                RowPadding = 0
-            };
+            reminderListPage.ClearChildren();
 
             //Create button
             var createButton = new Button(Game1.mouseCursors, new(384, 396, 15, 15))
             {
-                Size = new(Width + 40, reminderListPage.RowHeight + 4),
-                Callback = (e) => { newReminder = new(); },
-                LocalPosition = new(-20, 0)
+                DrawSize = new(Width + 40, reminderListPage.RowHeight + 4),
+                OnClick = (e) => { newReminder = new(); },
+                Position = new(-20, 0)
             };
 
-            var createButtonPlus = new Image()
-            {
-                Texture = Game1.mouseCursors,
-                TextureArea = new(1, 412, 14, 14),
-            };
-            createButtonPlus.LocalPosition = new((createButton.Width - createButtonPlus.Width) / 2, 36);
+            var createButtonPlus = new Image(Game1.mouseCursors, new(1, 412, 14, 14));
+            createButtonPlus.Position = new((createButton.Width - createButtonPlus.Width) / 2, 36);
 
             reminderListPage.AddRow(new Element[] { createButton, createButtonPlus });
 
@@ -98,63 +108,56 @@ namespace ValleyReminders
             {
                 var button = new Button(Game1.mouseCursors, new(384, 396, 15, 15))
                 {
-                    Size = new(Width + 40, reminderListPage.RowHeight + 4),
-                    Callback = (e) => { selectedReminder = reminder; },
-                    LocalPosition = new(-20, 0)
+                    DrawSize = new(Width + 40, reminderListPage.RowHeight + 4),
+                    OnClick = (e) => { selectedReminder = reminder; },
+                    Position = new(-20, 0)
                 };
 
-                var textBox = new Label()
+                var TextInput = new Text()
                 {
                     String = reminder.Name.Truncate(34),
                     Bold = true,
-                    LocalPosition = new(0, 36)
+                    Position = new(0, 36)
                 };
 
                 var enabledCheck = new Checkbox()
                 {
                     Checked = reminder.Enabled,
-                    Callback = (e) => { reminder.Enabled = ((Checkbox)e).Checked; },
+                    OnClick = (e) => { reminder.Enabled = ((Checkbox)e).Checked; },
                 };
-                enabledCheck.LocalPosition = new(Width - 40, (reminderListPage.RowHeight - enabledCheck.Height) / 2 -24);
+                enabledCheck.Position = new(Width - 40, (reminderListPage.RowHeight - enabledCheck.Height) / 2 -24);
 
                 var deleteButton = new Button(Game1.mouseCursors, new(323, 433, 9, 10))
                 {
-                    Callback = (e) => { reminders.Remove(reminder); reminderListDirty = true; },
-                    BoxDraw = false
+                    OnClick = (e) => { reminders.Remove(reminder); reminderListDirty = true; },
+                    DrawToFit = false
                 };
-                deleteButton.LocalPosition = new(Width - 40, (reminderListPage.RowHeight - deleteButton.Height) / 2 + 22);
+                deleteButton.Position = new(Width - 40, (reminderListPage.RowHeight - deleteButton.Height) / 2 + 22);
 
-                reminderListPage.AddRow(new Element[] { button, textBox, enabledCheck, deleteButton });
+                reminderListPage.AddRow(new Element[] { button, TextInput, enabledCheck, deleteButton });
             }
-            rootElement.AddChild(reminderListPage);
             reminderListDirty = false;
         }
 
         private void UpdateEditPage(Reminder selectedReminder)
         {
-            rootElement.RemoveChild(reminderEditPage);
+            reminderEditPage.ClearChildren();
 
-            reminderEditPage = new()
-            {
-                Size = new(Width, Height),
-                LocalPosition = CenterOffset,
-            };
-
-            var msgTextBox = new Textbox()
+            var msgTextInput = new TextInput()
             {
                 String = selectedReminder.Message,
-                Callback = (e) =>
+                OnValueChange = (e) =>
                 {
                     selectedReminder.Message = e.String;
                     reminderListDirty = true;
                 }
             };
-            reminderEditPage.AddChild(msgTextBox);
+            reminderEditPage.AddChild(msgTextInput);
 
             var enabledCheckbox = new Checkbox()
             {
                 Checked = selectedReminder.Enabled,
-                Callback = (e) =>
+                OnClick = (e) =>
                 {
                     selectedReminder.Enabled = (e as Checkbox)!.Checked;
                     reminderListDirty = true;
@@ -167,7 +170,7 @@ namespace ValleyReminders
             {
                 RowHeight = 60,
                 Size = new(width, height),
-                RowSlip = 0
+                RowOffset = 0
             };
             const int parameterMargin = 25; //for when condition input will be updated in reverse
 
@@ -176,7 +179,7 @@ namespace ValleyReminders
             //Display conditions
             foreach (var cond in selectedReminder.Conditions)
             {
-                var conditionName = new Label()
+                var conditionName = new Text()
                 {
                     String = Regex.Replace(cond.MethodName, "([A-Z]+(?=$|[A-Z][a-z])|[A-Z]?[a-z]+)", " $1"),
                     Font = Game1.smallFont
@@ -191,11 +194,11 @@ namespace ValleyReminders
                 {
                     if (Conditions.GetParameterCount(cond.MethodName) > 1)
                     {
-                        var condParameterLabel = new Label()
+                        var condParameterLabel = new Text()
                         {
                             String = condParameter.Key,
                             Font = Game1.smallFont,
-                            LocalPosition = new(previousWidth + 25, 0)
+                            Position = new(previousWidth + 25, 0)
                         };
                         previousWidth += condParameterLabel.Width + 25;
                         row.Add(condParameterLabel);
@@ -205,40 +208,40 @@ namespace ValleyReminders
                     switch (condParameter.Value)
                     {
                         case ParameterType.String:
-                            Textbox condParameterTextBox = new()
+                            TextInput condParameterTextInput = new()
                             {
                                 String = cond.ParameterValues[i],
-                                Callback = (e) =>
+                                OnValueChange = (e) =>
                                 {
                                     cond.ParameterValues[parameterIndex] = e.String;
                                 },
-                                LocalPosition = new(previousWidth + 25, 0)
+                                Position = new(previousWidth + 25, 0)
                             };
-                            previousWidth += condParameterTextBox.Width + 25;
-                            row.Add(condParameterTextBox);
+                            previousWidth += condParameterTextInput.Width + 25;
+                            row.Add(condParameterTextInput);
                             break;
                         case ParameterType.Int:
-                            Intbox condParameterIntBox = new()
+                            IntInput condParameterIntBox = new()
                             {
                                 String = cond.ParameterValues[i],
-                                Callback = (e) =>
+                                OnValueChange = (e) =>
                                 {
                                     cond.ParameterValues[parameterIndex] = e.String;
                                 },
-                                LocalPosition = new(previousWidth + 25, 0)
+                                Position = new(previousWidth + 25, 0)
                             };
                             previousWidth += condParameterIntBox.Width + 25;
                             row.Add(condParameterIntBox);
                             break;
                         case ParameterType.Float:
-                            Floatbox condParameterFloatBox = new()
+                            FloatInput condParameterFloatBox = new()
                             {
                                 String = cond.ParameterValues[i],
-                                Callback = (e) =>
+                                OnValueChange = (e) =>
                                 {
                                     cond.ParameterValues[parameterIndex] = e.String;
                                 },
-                                LocalPosition = new(previousWidth + 25, 0)
+                                Position = new(previousWidth + 25, 0)
                             };
                             previousWidth += condParameterFloatBox.Width + 25;
                             row.Add(condParameterFloatBox);
@@ -256,43 +259,32 @@ namespace ValleyReminders
             //Menu buttons
             var backButton = new Button(Game1.mouseCursors, new(352, 495, 12, 11))
             {
-                Size = new(48, 44),
-                LocalPosition = new Vector2(-96, -22),
-                Callback = (e) => { this.selectedReminder = null; }
+                DrawSize = new(48, 44),
+                Position = new Vector2(-96, -22),
+                OnClick = (e) => { this.selectedReminder = null; }
             };
             reminderEditPage.AddChild(backButton);
-
-            rootElement.AddChild(reminderEditPage);
         }
 
         private void UpdateCreatePage()
         {
-            rootElement.RemoveChild(reminderCreatePage);
-
-            reminderCreatePage = new()
-            {
-                Size = new(Width, Height),
-                LocalPosition = CenterOffset,
-                OutlineColor = Color.White,
-            };
+            reminderCreatePage.ClearChildren();
 
             var backButton = new Button(Game1.mouseCursors, new(352, 495, 12, 11))
             {
-                Size = new(48, 44),
-                LocalPosition = new Vector2(-96, -22),
-                Callback = (e) => { newReminder = null; }
+                DrawSize = new(48, 44),
+                Position = new Vector2(-96, -22),
+                OnClick = (e) => { newReminder = null; }
             };
             reminderCreatePage.AddChild(backButton);
 
             var createButton = new Button(Game1.mouseCursors, new(341, 410, 23, 9))
             {
-                Size = new(46, 18),
-                LocalPosition = new Vector2(20, 100),
-                Callback = (e) => { reminders.Add(newReminder!); reminderListDirty = true; newReminder = null; }
+                DrawSize = new(46, 18),
+                Position = new Vector2(20, 100),
+                OnClick = (e) => { reminders.Add(newReminder!); reminderListDirty = true; newReminder = null; }
             };
             reminderCreatePage.AddChild(createButton);
-
-            rootElement.AddChild(reminderCreatePage);
         }
 
         public override void update(GameTime time)
@@ -300,73 +292,43 @@ namespace ValleyReminders
             base.update(time);
 
             //Self updating state. Feels a bit messy
-            if(selectedReminder != null && state != ReminderMenuState.EDIT)
+            if (selectedReminder != null && state != ReminderMenuState.EDIT)
             {
                 state = ReminderMenuState.EDIT;
                 UpdateEditPage(selectedReminder);
+
+                reminderListPage.Enabled = false;
+                reminderEditPage.Enabled = true;
+                reminderCreatePage.Enabled = false;
             }
             else if (newReminder != null && state != ReminderMenuState.CREATE)
             {
                 state = ReminderMenuState.CREATE;
                 UpdateCreatePage();
                 selectedReminder = null;
+
+                reminderListPage.Enabled = false;
+                reminderEditPage.Enabled = false;
+                reminderCreatePage.Enabled = true;
             }
             else if (selectedReminder == null && newReminder == null)
             {
                 state = ReminderMenuState.LIST;
                 if (reminderListDirty) { UpdateListPage(); }
+
+                reminderListPage.Enabled = true;
+                reminderEditPage.Enabled = false;
+                reminderCreatePage.Enabled = false;
             }
-
-            reminderListPage.Enabled = false;
-            reminderEditPage.Enabled = false;
-            reminderCreatePage.Enabled = false;
-
-            switch (state)
-            {
-                case ReminderMenuState.LIST:
-                    reminderListPage.Enabled = true;
-                    break;
-                case ReminderMenuState.EDIT:
-                    reminderEditPage.Enabled = true;
-                    break;
-                case ReminderMenuState.CREATE:
-                    reminderEditPage.Enabled = true;
-                    break;
-                default:
-                    break;
-            }
-
-            rootElement.Update();
         }
 
         public override void receiveScrollWheelAction(int direction)
         {
             base.receiveScrollWheelAction(direction);
-            reminderListPage.Scrollbar.ScrollBy(direction);
-        }
-
-        public override void draw(SpriteBatch b)
-        {
-            switch (state)
+            if(reminderListPage.Enabled)
             {
-                case ReminderMenuState.LIST:
-                    reminderListPage.Draw(b);
-                    break;
-                case ReminderMenuState.EDIT:
-                    reminderEditPage.Draw(b);
-                    break;
-                case ReminderMenuState.CREATE:
-                    reminderCreatePage.Draw(b);
-                    break;
-                default:
-                    break;
+               // reminderListPage.Scrollbar.ScrollBy(direction);
             }
-
-            if (shouldDrawCloseButton())
-            {
-                upperRightCloseButton.draw(b);
-            }
-            drawMouse(b);
         }
     }
 }
